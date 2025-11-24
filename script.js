ad211e97ba1fa55fc4853da916be6cfb15c3e2ac
Lee Massey <lee.m.massey@gmail.com>
2025-11-23 18:55:15 -0800

    Add basic operation functions to script.js

--- script.js
+++ script.js
@@ -2,6 +2,22 @@ const buttons = document.querySelectorAll('button');
 
 buttons.forEach((button) => {
     button.addEventListener('click', function() {
-        alert(this.textContent);
+        alert();
     })
 });
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
