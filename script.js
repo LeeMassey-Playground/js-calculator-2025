0da804a95dfbc81883db9a9aaaad7cd437890280
Lee Massey <lee.m.massey@gmail.com>
2025-11-28 23:48:34 -0800

    Fix minor bugs (clicking operator buttons multiple times in a row), fix edge cases (handling leading zeroes, only allowing 1 decimal point per input value

--- script.js
+++ script.js
@@ -1,93 +1,154 @@
-const displayA = document.querySelector('#display-a');
-const displayB = document.querySelector('#display-b');
+function calculator() {
 
-const buttonClear = document.querySelector('#button-clear');
-const buttonDelete = document.querySelector('#button-delete');
-const buttonsOperator = document.querySelectorAll('.button-operator');
-const buttonEquals = document.querySelector('#button-equals');
-const buttonsNumbers = document.querySelectorAll('.button-number');
-const buttonDecimal = document.querySelector('#button-decimal');
+    const displayA = document.querySelector('#display-a');
+    const displayB = document.querySelector('#display-b');
 
-let a = '';
-let b = '';
-let operator = '';
-let register = '';
-let result = '';
-let equals = '';
+    const buttonClear = document.querySelector('#button-clear');
+    const buttonDelete = document.querySelector('#button-delete');
+    const buttonsOperator = document.querySelectorAll('.button-operator');
+    const buttonEquals = document.querySelector('#button-equals');
+    const buttonsNumbers = document.querySelectorAll('.button-number');
+    const buttonDecimal = document.querySelector('#button-decimal');
 
-function updateDisplay() {
-    displayA.textContent = register;
-    displayB.textContent = `${a} ${operator} ${b} ${equals}`;
+    let a = '';
+    let b = '';
+    let operator = '';
+    let register = '';
+    let result = '';
+    let equals = '';
 
-    console.log(`a: ${a} op: ${operator} b: ${b} result: ${result} register: ${register}`);
-}
+    function debug() {
+        console.log(`a: ${a} op: ${operator} b: ${b} result: ${result} register: ${register}`);
+    }
 
-updateDisplay();
+    function updateDisplay() {
+        if (result) {
+            equals = ' = ';
+        }
 
-buttonsNumbers.forEach((button) => {
-    button.addEventListener('click', appendRegister);
-});
+        displayA.textContent = register;
+        displayB.textContent = `${a} ${operator} ${b} ${equals}`;
 
-buttonsOperator.forEach((button) => {
-    button.addEventListener('click', selectOperator);
-});
+        debug();
+    }
 
-buttonEquals.addEventListener('click', operate);
+    buttonsNumbers.forEach((button) => {
+        button.addEventListener('click', appendRegister);
+    });
 
-function appendRegister() {
-    register += this.textContent;
-    updateDisplay();
-}
+    buttonsOperator.forEach((button) => {
+        button.addEventListener('click', selectOperator);
+    });
 
-function selectOperator() {
-    if(!operator) {
-        operator = this.textContent;
-        a = register;
-        b = '';
-        register = '';
+    buttonEquals.addEventListener('click', operate);
+
+    buttonClear.addEventListener('click', clear);
+
+    buttonDelete.addEventListener('click', backspace);
+
+    function appendRegister() {
+        if (result) {
+            clear();
+        }
+
+        if (register.includes('.') && this.textContent === '.') {
+            return null;
+        }
+
+        if (register.length === 1 && register.includes('0') && this.textContent === '0') {
+            return null;
+        }
+
+        if (!register && this.textContent === '.') {
+            register += '0';
+        }
+
+        register += this.textContent;
+
         updateDisplay();
     }
-    else if(a && register) {
-        operate();
+
+    function selectOperator() {
+        if (a && operator && register) {
+            result = '';
+            operate();
+        }
+
+        if (register) {
+            result = '';
+            a = register;
+            register = '';
+            operator = this.textContent;
+            updateDisplay();
+        }
     }
-}
 
-function operate() {
-    b = register;
-    let numA = Number(a);
-    let numB = Number(b);
-    switch (operator) {
-        case '+':
-            result = add(numA, numB);
-            break;
-        case '-':
-            result = subtract(numA, numB);
-            break;
-        case '*':
-            result = multiply(numA, numB);
-            break;
-        case '/':
-            result = divide(numA, numB);
+    function operate() {
+        if (!result && a && operator && register) {
+            b = register;
+            let numA = Number(a);
+            let numB = Number(b);
+
+            switch (operator) {
+                case '+':
+                    result = add(numA, numB);
+                    break;
+                case '-':
+                    result = subtract(numA, numB);
+                    break;
+                case '*':
+                    result = multiply(numA, numB);
+                    break;
+                case '/':
+                    result = divide(numA, numB);
+            }
+
+            register = result.toString();
+            
+            updateDisplay();
+
+            a = '';
+            b = '';
+            operator = '';
+            equals = '';
+
+            debug();
+        }
     }
-    register = result;
-    equals = ' = ';
-    updateDisplay();
-    operator = '';
-    equals = '';
+
+    function clear() {
+        a = '';
+        b = '';
+        operator = '';
+        register = '';
+        result = '';
+        equals = '';
+
+        debug();
+        updateDisplay();
+    }
+
+    function backspace() {
+        register = register.slice(0, -1);
+        displayA.textContent = register;
+    }
+
+    function add(x, y) {
+        return x + y;
+    }
+
+    function subtract(x, y) {
+        return x - y;
+    }
+
+    function multiply(x, y) {
+        return x * y;
+    }
+
+    function divide(x, y) {
+        return x / y;
+    }
+
 }
 
-function add(x, y) {
-    return x + y;
-}
-
-function subtract(x, y) {
-    return x - y;
-}
-
-function multiply(x, y) {
-    return x * y;
-}
-
-function divide(x, y) {
-    return x / y;
-}
+calculator();
